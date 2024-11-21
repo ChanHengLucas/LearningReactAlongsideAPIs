6b4ffb38fce791f4aa18f18961c5d9c25a65d5ad
ChanHengLucas <91720922+ChanHengLucas@users.noreply.github.com>
2024-11-21 12:07:17 -0500

    yftugihjklop87t6rtdyfuioutdrsedyhioutrdyruadst8970iyufdtuidfzsgyutidsfzyu9ytfdsfzty8otrsfazduiyopoyfdgrstuiyoyfdtyuoifhtdiuokgfh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h2>React JSON</h2>
+      <Home />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
